14c07b463a2bf860b7700bccca22805646020247
Shahmeer-09 <shahmeerweb@gmail.com>
2024-07-16 14:19:32 +0500

    added env

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools initialIsOpen={false}   />
+        {/* <ReactQueryDevtools initialIsOpen={false}   /> */}
       </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>
